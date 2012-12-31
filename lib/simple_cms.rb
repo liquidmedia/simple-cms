02b4ad081bf41855f9efcc2585913f392b2d5b4e
Jeremie Wood <jeremie@jeremiewood.com>
2012-12-31 17:22:21 -0500

    Fun with assets

--- lib/simple_cms.rb
+++ lib/simple_cms.rb
@@ -4,10 +4,14 @@ module SimpleCms
   mattr_accessor :layout
   self.layout = 'application'
 
-  #def self.assets
-  #  asset_list = Dir[root.join('vendor/assets/javascripts/ckeditor/**', '*.{js,css}')].inject([]) do |list, path|
-  #    list << Pathname.new(path).relative_path_from(root_path.join('vendor/assets/javascripts'))
-  #  end
-  #  asset_list
-  #end
+  def self.assets
+    asset_list = Dir[SimpleCms.root_path.join('vendor/assets/javascripts/ckeditor/**', '*.{js,css}')].inject([]) do |list, path|
+      list << Pathname.new(path).relative_path_from(SimpleCms.root_path.join('vendor/assets/javascripts'))
+    end
+    asset_list
+  end
+
+  def self.root_path
+    @root_path ||= Pathname.new( File.dirname(File.expand_path('../', __FILE__)) )
+  end
 end
--- lib/simple_cms/engine.rb
+++ lib/simple_cms/engine.rb
@@ -2,12 +2,8 @@ module SimpleCms
   class Engine < ::Rails::Engine
     engine_name 'simple_cms'
 
-    #initializer "simple_cms.assets_precompile" do |app|
-    #  app.config.assets.precompile += SimpleCms.assets
-    #end
-
-    initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/vendor/assets"
+    initializer "simple_cms.assets_precompile" do |app|
+      app.config.assets.precompile += SimpleCms.assets
     end
   end
 end
