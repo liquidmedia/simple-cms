1a78815594e777f52645053e9d36825025d63487
Jeremie Wood <jeremie@jeremiewood.com>
2014-02-03 17:47:21 -0500

    Bugfix - non-admin users don't see default text

--- app/helpers/simple_cms/application_helper.rb
+++ app/helpers/simple_cms/application_helper.rb
@@ -1,14 +1,15 @@
 module SimpleCms
   module ApplicationHelper
 
-    def page_fragment(name, user = nil, tag_name = :div, default_text = "This text block is blank. Click on it to edit it and add content.")
-      @page = Page.find_or_create_by_name_and_locale(name, I18n.locale, :url => request.path[1..-1], :full_url => request.url)
+    def page_fragment(name, user = nil, tag_name = :div, default_text = nil)
+      @page = Page.find_or_create_by_name_and_locale(name, I18n.locale, :url => request.path[1..-1], :full_url => request.url, :content => default_text)
       @page.update_attributes(:url => request.path[1..-1]) if @page.url.nil?
       @page.users << user unless user.nil? || @page.users.include?(user)
 
       content = @page.content
-      return '' if content.blank? && !(admin_signed_in? || @page.editable?(current_user))
       content = default_text if content.blank?
+      return '' if content.blank? && !(admin_signed_in? || @page.editable?(current_user))
+      content = 'This text block is blank. Click on it to edit it and add content.' if content.blank?
 
       content_tag :div, :class => :page_fragment do
         content_tag tag_name, :class => :editable_area, :id => "page_fragment_#{@page.id}", :"data-id" => @page.id do
