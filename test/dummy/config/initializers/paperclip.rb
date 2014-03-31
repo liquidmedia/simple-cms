6b317aa38f017898680fd3efdc8bf7fbb9028263
Paul Doerwald <paul@liquidmedia.ca>
2014-03-31 16:01:48 -0300

    Remove the offending amazon s3 access key information

--- test/dummy/config/initializers/paperclip.rb
+++
@@ -1,6 +0,0 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:s3_credentials] = {
-    :bucket => 'liquidmedia-lta-files-development-jeremie',
-    :access_key_id => 'AKIAJD5QR62OCYY4I7AA',
-    :secret_access_key => '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'
-}
